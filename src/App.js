5ab1a3d2bfb2b170e80b2ca1cc4e06abcff4e383
Oleh Voroniuk <voroniuk@Olehs-MacBook-Pro.local>
2025-03-06 20:13:24 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import {List} from './components/List';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <h1>Hello World</h1>
+        </div>
+      </div>
+      <List />
+    </div>
+  );
+}
+
+export default App;
